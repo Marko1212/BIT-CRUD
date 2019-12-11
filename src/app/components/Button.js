21a48086055050a5eea8db4cac8885d6daf04714
Your Name <you@example.com>
2019-12-11 07:50:04 +0100

    small update delete 2 files

--- src/app/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Button(props) {
-    return (
-        <button className={props.className} type="button" onChange={props.onChange} >{props.children}</button>
-    );
-}
-
-export default Button;
--- src/app/components/Input.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Input(props) {
-    return (
-        <input className={props.className} type="text" />
-    );
-}
-
-export default Input;
