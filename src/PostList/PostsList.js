ddea3dce4bb8a41efe9241dffff6ebbac05c5529
Your Name <you@example.com>
2019-12-13 08:21:06 +0100

    added All posts title

--- src/PostList/PostsList.js
+++ src/PostList/PostsList.js
@@ -37,13 +37,28 @@ class PostsList extends Component {
     render() {  
         console.log(this.state.posts)
         return ( 
+
+            <div className="container">
+            <div className="row">
+                <h1 class="title">All posts</h1>
+            </div>
+            <div className="row">
+
+                <div className="postList">
+                    {this.state.posts.map((post, i) =>
+                        <CardPost key={i} {...post} />
+                    )}
+                </div>
+
+            </div>
+        </div>
             
-            <div className="postList">
+/*             <div className="postList">
                 {this.state.posts.map((post, i) => 
                     <Post key={i} {...post} />
                 )}
                 
-            </div>
+            </div> */
             
          );
     }
