3e11c158c3c2f813a199edc03b4bb469dee38d7e
Marko1212 <shihonage7@gmail.com>
2020-05-19 09:19:39 +0200

    correction

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import App from './App';
 import { HashRouter } from "react-router-dom";
 /* import 'bootstrap/dist/css/bootstrap.min.css'; */
 
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+ReactDOM.render(<HashRouter>
+  <App />
+</HashRouter>,
   document.getElementById('root')
 );
 
